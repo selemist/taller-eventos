65008c9fed1ec179f760514aa94dd471695aa81a
selemist <168042467+selemist@users.noreply.github.com>
2024-08-12 23:25:23 -0300

    Arreglos finales al código para adjuntar la solucíon

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
 document.getElementById('divbonito').addEventListener('click', function(){
     alert('Hola! Soy el Div');
-})
+});
+//Esto es para el div //
+var button = document.getElementById('botoncito');
+
+//esto es para definir que la variable button es el id con el nombre botoncito
+
+button.addEventListener('click', function(event){
+    event.stopPropagation();
+});
